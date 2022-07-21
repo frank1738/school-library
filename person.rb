fe023b847f8bc56768fe93d6f50b3988d342b987
frank1738 <franklineosoro08@gmail.com>
2022-07-21 10:53:21 +0300

    add basic UI

--- person.rb
+++ person.rb
@@ -4,8 +4,8 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super
-    @id = id
+    super()
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
